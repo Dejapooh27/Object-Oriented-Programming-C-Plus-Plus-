36939f6e328708ba9628d146ed7d7e2a67525af5
Dejapooh27 <monet13245@gmail.com>
2024-02-06 12:56:35 -0800

    lab2

---
+++ Lab2/Lab2_calc.cpp
@@ -0,0 +1,18 @@
+//Lab2_calc.cpp
+/***
+ * {Lab 2 teaches us different ways to represent numbers and objects in relation to memory}
+***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    //numerical
+    int int_x = 1;
+    int int_y = 2;
+    double double_z = 2.0;
+
+    cout << "int divided by int: " << int_x / int_y << endl;
+    cout << "int divded by double: " << int_x / double_z << endl;
+}
