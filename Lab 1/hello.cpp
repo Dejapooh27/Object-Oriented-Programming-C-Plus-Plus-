a3515ce8dfa9a69a72e777309d4f5faa358e8dec
Dejapooh27 <monet13245@gmail.com>
2024-02-06 12:46:36 -0800

    lab 1

---
+++ Lab 1/hello.cpp
@@ -0,0 +1,14 @@
+/***
+ * {Lab One teaches us how to set up our computer to run c++ code along with how to navigate our terminal}
+ ***/
+ 
+ #include <iostream>
+ #include <cstdlib>
+ using namespace std;
+
+ int main () {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+
+ }
